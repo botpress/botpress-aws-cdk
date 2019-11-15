8177e00871fc2d9e7005e71cf7e8904f4a1e43b7
Simon-Pierre Gingras <simon.pierre.gingras@gmail.com>
2019-11-15 08:42:39 -0500

    Initial project

---
+++ test/tempdir.test.ts
@@ -0,0 +1,13 @@
+import { expect as expectCDK, matchTemplate, MatchStyle } from '@aws-cdk/assert';
+import cdk = require('@aws-cdk/core');
+import Tempdir = require('../lib/tempdir-stack');
+
+test('Empty Stack', () => {
+    const app = new cdk.App();
+    // WHEN
+    const stack = new Tempdir.TempdirStack(app, 'MyTestStack');
+    // THEN
+    expectCDK(stack).to(matchTemplate({
+      "Resources": {}
+    }, MatchStyle.EXACT))
+});
