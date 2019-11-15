a5b688e4135122a7313663256aa78a519d79771f
Simon-Pierre Gingras <simon.pierre.gingras@gmail.com>
2019-11-15 09:53:48 -0500

    Can load sample app

--- lib/mystack.ts
+++ lib/mystack.ts
@@ -1,9 +1,29 @@
-import cdk = require('@aws-cdk/core');
+import cdk = require("@aws-cdk/core");
+import ec2 = require("@aws-cdk/aws-ec2");
+import ecs = require("@aws-cdk/aws-ecs");
+import ecsPatterns = require("@aws-cdk/aws-ecs-patterns");
 
 export class MyStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const vpc = new ec2.Vpc(this, "VPC");
+
+    const cluster = new ecs.Cluster(this, "Cluster", {
+      vpc
+    });
+
+    const loadBalancedFargateService = new ecsPatterns.ApplicationLoadBalancedFargateService(
+      this,
+      "Service",
+      {
+        cluster,
+        memoryLimitMiB: 1024,
+        cpu: 512,
+        taskImageOptions: {
+          image: ecs.ContainerImage.fromRegistry("amazon/amazon-ecs-sample")
+        }
+      }
+    );
   }
 }
