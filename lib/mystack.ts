fe2d9f1f83f6daaafed0e091a101823eb0ecccfe
Simon-Pierre Gingras <simon.pierre.gingras@gmail.com>
2019-11-15 11:06:12 -0500

    Custom container definition

--- lib/mystack.ts
+++ lib/mystack.ts
@@ -2,6 +2,7 @@ import cdk = require("@aws-cdk/core");
 import ec2 = require("@aws-cdk/aws-ec2");
 import ecs = require("@aws-cdk/aws-ecs");
 import ecsPatterns = require("@aws-cdk/aws-ecs-patterns");
+import { ContainerDefinition } from "@aws-cdk/aws-ecs";
 
 export class MyStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
@@ -13,17 +14,25 @@ export class MyStack extends cdk.Stack {
       vpc
     });
 
+    const taskDefinition = new ecs.FargateTaskDefinition(this, "TaskDef", {
+      memoryLimitMiB: 512,
+      cpu: 256
+    });
+    const container = taskDefinition.addContainer("MyContainer", {
+      image: ecs.ContainerImage.fromRegistry("botpress/server:v12_2_2")
+    });
+    container.addPortMappings({
+      containerPort: 3000
+    });
+
     const loadBalancedFargateService = new ecsPatterns.ApplicationLoadBalancedFargateService(
       this,
       "Service",
       {
         cluster,
-        memoryLimitMiB: 1024,
-        cpu: 512,
-        taskImageOptions: {
-          image: ecs.ContainerImage.fromRegistry("botpress/server:v12_2_2"),
-          containerPort: 3000
-        }
+        // memoryLimitMiB: 1024,
+        // cpu: 512,
+        taskDefinition
       }
     );
 
