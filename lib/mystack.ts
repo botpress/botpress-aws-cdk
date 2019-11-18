c4b7aba4450f1d62c98c8808f8b45156e150f007
Simon-Pierre Gingras <simon.pierre.gingras@gmail.com>
2019-11-18 16:16:38 -0500

    Added db cluster, inbound connection

--- lib/mystack.ts
+++ lib/mystack.ts
@@ -2,12 +2,37 @@ import cdk = require("@aws-cdk/core");
 import ec2 = require("@aws-cdk/aws-ec2");
 import ecs = require("@aws-cdk/aws-ecs");
 import ecsPatterns = require("@aws-cdk/aws-ecs-patterns");
+import rds = require("@aws-cdk/aws-rds");
 
 export class MyStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     const vpc = new ec2.Vpc(this, "VPC");
+
+    const dbName = "botpressdb";
+    const dbCluster = new rds.DatabaseCluster(this, "Database", {
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      engine: rds.DatabaseClusterEngine.AURORA_POSTGRESQL,
+      defaultDatabaseName: dbName,
+      instances: 1,
+      masterUser: {
+        username: "clusteradmin"
+      },
+      instanceProps: {
+        instanceType: ec2.InstanceType.of(
+          ec2.InstanceClass.BURSTABLE3,
+          ec2.InstanceSize.MEDIUM
+        ),
+        vpcSubnets: {
+          subnetType: ec2.SubnetType.PRIVATE
+        },
+        vpc
+      },
+      parameterGroup: {
+        parameterGroupName: "default.aurora-postgresql10"
+      } as any
+    });
 
     const cluster = new ecs.Cluster(this, "Cluster", {
       vpc
@@ -38,6 +63,11 @@ export class MyStack extends cdk.Stack {
         taskDefinition,
         healthCheckGracePeriod: cdk.Duration.seconds(600)
       }
+    );
+
+    dbCluster.connections.allowFrom(
+      loadBalancedFargateService.service,
+      ec2.Port.allTraffic()
     );
 
     loadBalancedFargateService.targetGroup.configureHealthCheck({
